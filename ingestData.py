1aed949d2f4254db1ab1251088135c0005b5eedd
Sup3rRookie <dev.akaun@gmail.com>
2021-08-31 16:55:22 +0800

    Post test data to the API Gateway

--- ingestData.py
+++ ingestData.py
@@ -14,3 +14,14 @@ data = pd.read_csv('testData.csv', sep = ',')
 export = data.loc[2].to_json()
 response = requests.post(URL, data = export)
 print(response)
+
+# Write all the rows from the testfile to the api as 'Put' Request
+for i in data.index:
+    # convert row to json
+    export = data.loc[i].to_json()
+
+    # send it to the api
+    response = requests.post(URL, data = export)
+
+    # print the return code
+    print(response)
