a96f13788f9f48f3e645680e4cf4c2fe3be90ae6
Sup3rRookie <dev.akaun@gmail.com>
2021-08-31 10:07:22 +0800

    Added Post Method to LamdaCode

--- lambdaCode.py
+++ lambdaCode.py
@@ -6,8 +6,8 @@ def lambda_handler(event, context):
     print('My Event:')
     print(event)
 
-# my_context = event.get('context')
-# method = my_context.get('http-method')
+    # my_context = event.get('context')
+    # method = my_context.get('http-method')
     method = event['context']['http-method']
 
     if method == "GET":
@@ -23,3 +23,25 @@ def lambda_handler(event, context):
             'body' : json.dumps(response['Item'])
         }
 
+    elif method == "POST":
+
+        p_record = event['body-json']
+        recordstring = json.dumps(p_record)
+
+        client = boto3.client('kinesis')
+        response = client.put_record(
+            StreamName = 'APIData',
+            Data = recordstring,
+            PartitionKey = 'string'
+        )
+
+        return {
+            'statusCode' :200,
+            'body' : json.dumps(p_record)
+        }
+
+    else:
+        return{
+            'statusCode' :200,
+            'body' : json.dumps('Server Error')
+        }
