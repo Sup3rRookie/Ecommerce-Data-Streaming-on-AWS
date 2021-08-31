e8eca3e66e3d3006877d50c27c3abe6f7b3b4537
Sup3rRookie <dev.akaun@gmail.com>
2021-08-31 10:03:18 +0800

    Added Get method to lamdaCode

--- lambdaCode.py
+++ lambdaCode.py
@@ -6,10 +6,20 @@ def lambda_handler(event, context):
     print('My Event:')
     print(event)
 
-my_context = event.get('context')
-method = my_context.get('http-method')
-method = event['context']['http-method']
+# my_context = event.get('context')
+# method = my_context.get('http-method')
+    method = event['context']['http-method']
 
-if method == 'GET':
-    dynamo_client = boto3.client('dynamodb')
+    if method == "GET":
+
+        im_customer_ID = event['params']['querystring']['CustomerID']
+        print(im_customer_ID)
+
+        response = dynamo_client.get_itme(TableName = 'Customers', Key = {'CustomerID':{'N':im_customer_ID}})
+        print(response['Item'])
+
+        return {
+            'statusCode' :200,
+            'body' : json.dumps(response['Item'])
+        }
 
